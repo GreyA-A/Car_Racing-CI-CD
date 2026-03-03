8c8033827add1bf83fb26b022fda7f1f7adfcafc
Artur Kutsenko <a.kutsenko06@gmail.com>
2026-03-03 02:25:06 +0200

    Added pygame.mask generation for roadside

--- track.py
+++ track.py
@@ -7,3 +7,11 @@ class Track:
 
         pygame.draw.rect(self.image, (100, 100, 100), (100, 100, width - 200, height - 200), border_radius=50)
         self.rect = self.image.get_rect()
+
+        self.mask = pygame.mask.from_surface(self.image)
+        self.mask.clear()
+        grass_color = (50, 200, 50)
+        for x in range(width):
+            for y in range(height):
+                if self.image.get_at((x, y))[:3] == grass_color:
+                    self.mask.set_at((x, y), 1)
