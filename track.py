53b04c40b5184e45be445f1b5f800c91e9dbf005
Artur Kutsenko <a.kutsenko06@gmail.com>
2026-03-03 02:38:25 +0200

    Added pygame.Rect array for checkpoint system and a timer in init

--- track.py
+++ track.py
@@ -15,6 +15,17 @@ class Track:
             for y in range(height):
                 if self.image.get_at((x, y))[:3] == grass_color:
                     self.mask.set_at((x, y), 1)
+
+        self.checkpoints = [
+            pygame.Rect(width // 2, 100, 10, 100),
+            pygame.Rect(width - 200, height // 2, 100, 10),
+            pygame.Rect(width // 2, height - 200, 10, 100),
+            pygame.Rect(100, height // 2, 100, 10)
+        ]
+        self.current_checkpoint = 0
+
+        self.start_time = pygame.time.get_ticks()
+        self.last_finish_time = 0
     
     def check_collision(self, car):
         car_mask = pygame.mask.from_surface(car.image)
