92574c4bc64a29d762029ad4143f30f49b6beb3b
Artur Kutsenko <a.kutsenko06@gmail.com>
2026-03-03 02:03:50 +0200

    Created class init w/ pos vectors

--- car.py
+++ car.py
@@ -2,4 +2,16 @@ import pygame
 import math
 
 class Car:
-    pass
+    def __init__(self, start_pos):
+        self.original_image = pygame.Surface((40, 20), pygame.SRCALPHA)
+        self.original_image.fill((255, 0, 0))  # Red car
+        self.image = self.original_image
+        self.rect = self.image.get_rect(center=start_pos)
+
+        self.pos = pygame.math.Vector2(start_pos)
+        self.angle = 0
+        self.speed = 0
+
+        self.max_speed = 5
+        self.acceleration = 0.2
+        self.rotation_speed = 4
